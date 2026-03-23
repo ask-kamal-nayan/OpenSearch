e3f3939a53d467f031d35489e25cbe03762afa90
Kamal Nayan <askkamal@amazon.com>
2026-03-23 14:09:46 +0530

    Minor import changes
    Signed-off-by: Kamal Nayan <askkamal@amazon.com>

--- server/src/main/java/org/opensearch/index/store/remote/CompositeRemoteDirectory.java
+++ server/src/main/java/org/opensearch/index/store/remote/CompositeRemoteDirectory.java
@@ -18,7 +18,6 @@ import org.opensearch.common.blobstore.BlobContainer;
 import org.opensearch.common.blobstore.BlobPath;
 import org.opensearch.common.blobstore.BlobStore;
 import org.opensearch.common.blobstore.BlobMetadata;
-import org.opensearch.common.blobstore.stream.write.WriteContext;
 import org.opensearch.common.blobstore.transfer.stream.OffsetRangeInputStream;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.index.engine.MergedSegmentWarmer;
