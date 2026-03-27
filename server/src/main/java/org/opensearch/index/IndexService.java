248fc51f5bcd24e4f1de657e231a2000d6f6ddf1
Kamal Nayan <askkamal@amazon.com>
2026-03-27 16:41:26 +0530

    Rebased with main
    Signed-off-by: Kamal Nayan <askkamal@amazon.com>

--- server/src/main/java/org/opensearch/index/IndexService.java
+++ server/src/main/java/org/opensearch/index/IndexService.java
@@ -475,6 +475,7 @@ public class IndexService extends AbstractIndexComponent implements IndicesClust
             (shardId) -> ReplicationStats.empty(),
             clusterDefaultMaxMergeAtOnce,
             clusterMergeSchedulerConfig,
+            null,
             null
         );
     }
